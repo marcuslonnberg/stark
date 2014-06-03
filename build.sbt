52d42ff920982cd2cee9fa9264774b91bebeb856
Marcus Lönnberg <marcus@lonnberg.nu>
2014-06-03 22:46:29 +0200

    Use Scala 2.11.1

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ organization := "se.marcuslonnberg"
 
 version := "0.1-SNAPSHOT"
 
+scalaVersion := "2.11.1"
+
 libraryDependencies ++= Dependencies.all
 
 resolvers ++= Dependencies.Resolvers.all
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,26 +2,26 @@ import sbt._
 
 object Dependencies {
   val Akka = {
-    val version = "2.3.2"
+    val version = "2.3.3"
     Seq("com.typesafe.akka" %% "akka-actor" % version,
       "com.typesafe.akka" %% "akka-slf4j" % version)
   }
 
   val Spray = {
-    val version = "1.3.1"
-    Seq("io.spray" % "spray-can" % version,
-      "io.spray" % "spray-client" % version,
-      "io.spray" % "spray-routing" % version,
-      "io.spray" % "spray-testkit" % version)
+    val version = "1.3.1-20140423"
+    Seq("io.spray" %% "spray-can" % version,
+      "io.spray" %% "spray-client" % version,
+      "io.spray" %% "spray-routing" % version,
+      "io.spray" %% "spray-testkit" % version)
   }
 
   val Json4s = Seq("org.json4s" %% "json4s-native" % "3.2.9")
 
-  val ScalaTest = Seq("org.scalatest" %% "scalatest" % "2.1.5" % "test")
+  val ScalaTest = Seq("org.scalatest" %% "scalatest" % "2.1.7" % "test")
 
   val TypeSafeConfig = {
     Seq("com.typesafe" % "config" % "1.2.0",
-      "net.ceedubs" %% "ficus" % "1.0.0")
+      "net.ceedubs" %% "ficus" % "1.1.1")
   }
 
   val Logback = Seq("ch.qos.logback" % "logback-classic" % "1.1.1")

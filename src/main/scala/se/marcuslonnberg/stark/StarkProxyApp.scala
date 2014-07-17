c01d367e415724fbf0b6dddbfd3a9e6306235385
Marcus Lönnberg <marcus@lonnberg.nu>
2014-07-17 09:40:39 +0200

    On start log if bound was successful or not

--- src/main/scala/se/marcuslonnberg/stark/StarkProxyApp.scala
+++ src/main/scala/se/marcuslonnberg/stark/StarkProxyApp.scala
@@ -1,7 +1,10 @@
 package se.marcuslonnberg.stark
 
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.io.IO
+import akka.pattern.ask
+import akka.util.Timeout
 import com.typesafe.config.{Config, ConfigFactory}
 import net.ceedubs.ficus.Ficus._
 import se.marcuslonnberg.stark.api.ApiActor
@@ -10,23 +13,27 @@ import spray.can.Http
 import spray.can.server.ServerSettings
 import spray.http.Uri
 
-import scala.collection.convert.wrapAsScala._
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
 
 object StarkProxyApp extends App with SSLSupport {
-  implicit val system = ActorSystem("proxy")
+  implicit val system = ActorSystem("stark")
+  import se.marcuslonnberg.stark.StarkProxyApp.system.dispatcher
   sys.addShutdownHook(system.shutdown())
 
+  val log = Logging(system, this.getClass)
   val conf = ConfigFactory.load()
 
   val proxies = system.actorOf(ProxiesActor.props(), "proxies")
-  val apiRoutingActor = system.actorOf(ApiActor.props(proxies), "api-routing")
+  val apiRouting = system.actorOf(ApiActor.props(proxies), "api-routing")
 
   val apiHost = Uri.Host(conf.as[String]("server.api-host"))
-  val connector = system.actorOf(ConnectActor.props(proxies, apiRoutingActor, apiHost), "connector")
+  val connector = system.actorOf(ConnectActor.props(proxies, apiRouting, apiHost), "connector")
 
-  val bindings = conf.getObjectList("server.bindings")
-  println(s"Found ${bindings.length} bindings in config")
-  bindings.foreach(b => bind(b.toConfig))
+  val bindings = conf.as[List[Config]]("server.bindings")
+  log.info(s"Found ${bindings.length} bindings in config")
+  implicit val timeout = Timeout(30.seconds)
+  bindings.foreach(bind)
 
   def bind(bindConfig: Config) = {
     val bindInterface = bindConfig.as[Option[String]]("interface").getOrElse(conf.as[String]("server.default-interface"))
@@ -47,6 +54,15 @@ object StarkProxyApp extends App with SSLSupport {
       }
     }
 
-    IO(Http) ! bind
+    (IO(Http) ? bind).onComplete {
+      case Success(Http.Bound(address)) =>
+        log.info("Bound to {}", address)
+      case Success(Http.CommandFailed(cmd)) =>
+        log.error("Could not bind to {}, reason: {}", bind.endpoint, cmd.failureMessage)
+      case Success(other) =>
+        log.error("Could not bind to {}, unknown response: {}", bind.endpoint, other)
+      case Failure(ex) =>
+        log.error(ex, "Could not bind to {}", bind.endpoint)
+    }
   }
 }
